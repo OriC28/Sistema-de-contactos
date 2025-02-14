157a649bdca3d9a9bd91e43946c87d4c1cc5b891
OriC28 <orianacolina.perea@gmail.com>
2025-02-14 19:16:30 -0400

    Select main file

--- views/main.php
+++ views/main.php
@@ -4,12 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
-=======
     <link rel="stylesheet" href="../assets/css/style.css?v=<?php echo time(); ?>">
-    
->>>>>>> 7415dd04a43722834727b14478bb04619f12bb2a
     <title>Contact System</title>
 </head>
 <body>
@@ -58,7 +53,6 @@
                 </script>   
             </section>
             <!-- MENSAJE DE ERROR TIPO WARNING -->
-<<<<<<< HEAD
             <div class="warning"></div>
         </section>
         <!-- CONTAINER TABLA DE CONTACTOS -->
@@ -67,18 +61,6 @@
                 <thead>
                     <tr>
                         <th>#</th>
-=======
-            <div class="warning">
-                <p>Contact not found.</p>
-            </div>
-        </section>
-        <!-- CONTAINER TABLA DE CONTACTOS -->
-        <section class="table-container">
-            <table>
-                
-                <thead>
-                    <tr>
->>>>>>> 7415dd04a43722834727b14478bb04619f12bb2a
                         <th>Names</th>
                         <th>Surnames</th>
                         <th>Company name</th>
@@ -88,7 +70,6 @@
                         <th>Options</th>
                     </tr>
                 </thead>
-<<<<<<< HEAD
                 <!--AQUÍ SE INSERTARÁN LOS DATOS-->
                 <tbody id="tbody"></tbody>
             </table>
@@ -109,42 +90,5 @@
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" 
     crossorigin="anonymous"></script>
     <script src="../assets/js/scripts.js"></script>
-=======
-                <tbody>
-                    <!-- DATOS INDIVIDUALES DE LOS ESTUDIANTES REGISTRADOS-->
-                <!-- <?php
-                    foreach ($students as $index => $data):
-                        $name =  htmlspecialchars(trim($data['primer_nombre_estudiante'])) . " " . htmlspecialchars(trim($data['segundo_nombre_estudiante'])) . " " 
-                                . htmlspecialchars(trim($data['primer_apellido_estudiante'])) . " " . htmlspecialchars(trim($data['segundo_apellido_estudiante']));
-                        $cedula = (int) trim($data['cedula']);
-                        $name_encrypted = base64_encode(openssl_encrypt($name, "aes-256-cbc", $key, 0, $iv));
-                        $ci_encrypted = base64_encode(openssl_encrypt($cedula, "aes-256-cbc", $key, 0, $iv));
-                    ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars(trim($data['cedula']));?></td>
-                        <td><?php echo htmlspecialchars(trim($data['primer_apellido_estudiante'])) . " " . htmlspecialchars(trim($data['segundo_apellido_estudiante']));?></td>
-                        <td><?php echo htmlspecialchars(trim($data['primer_nombre_estudiante'])) . " " . htmlspecialchars(trim($data['segundo_nombre_estudiante']));?></td>
-                        <td><?php echo htmlspecialchars(trim($data['correo_electronico']));?></td>
-                        <td>
-                            <div class="options">
-                                <button class="add-button" type="button"><a href="index.php?controller=studentData&action=getRowData&site=addnotes&id=<?= urlencode($ci_encrypted);?>&n=<?= urlencode($name_encrypted); ?>">Agregar</a></button>
-                                <button class="edit-button" type="button"><a href="index.php?controller=studentData&action=getRowData&site=editnotes&id=<?= urlencode($ci_encrypted);?>&n=<?= urlencode($name_encrypted); ?>">Editar</a></button>
-                            </div>
-                        </td>
-                    </tr>
-                <?php endforeach;?> -->
-                </tbody>
-            </table>
-            <div class="pagination">
-                <button><img width="15" height="15" src="https://img.icons8.com/ios-glyphs/30/less-than.png" alt="left"/></button>
-                <label for="">1/10</label>
-                <button><img width="15" height="15" src="https://img.icons8.com/ios-glyphs/30/more-than.png" alt="right"/></button>
-            </div>
-        </section>
-    </main>
-    <footer>
-        <p>footer</p>
-    </footer>
->>>>>>> 7415dd04a43722834727b14478bb04619f12bb2a
 </body>
 </html>
